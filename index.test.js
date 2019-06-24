5fe58de19116e64913ca63a73119a20a18090b03
Vasily93 <markovavasily@gmail.com>
2019-06-24 11:37:15 -0700

    same thing repeated

--- index.test.js
+++ index.test.js
@@ -1,12 +1,12 @@
 const dogs = require('./index.js');
 
 describe('dog create function', () => {
-    it('returns a dog object', () => {
-        const dog = dogs('spot', 5, '20lbs');
-        expect(dog).toEqual({
-            name:'spot',
-            age: 5,
-            weight: '20lbs'
-        });
+  it('returns a dog object', () => {
+    const dog = dogs('spot', 5, '20lbs');
+    expect(dog).toEqual({
+      name:'spot',
+      age: 5,
+      weight: '20lbs'
     });
+  });
 });
